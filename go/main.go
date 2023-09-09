0d742b375606ee995ea3a3c70febeefce669e5d2
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2023-09-08 21:51:42 -0300

    add secret

--- go/main.go
+++ go/main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/secret", secret)
 	http.HandleFunc("/health", health)
 	http.ListenAndServe(":8080", nil)
 }
@@ -17,6 +18,15 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	podName := os.Getenv("HOSTNAME")
 
 	res := fmt.Sprintf("<h1>Hello %s from pod %s!!!</h1>", name, podName)
+
+	w.Write([]byte(res))
+}
+
+func secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	res := fmt.Sprintf("<h1>User: %s | Password: %s</h1>", user, password)
 
 	w.Write([]byte(res))
 }
